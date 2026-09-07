fde6423aa7ab4db891e87fbe6e050bebca83020e
Aibel Jomon <aibeljomon7@gmail.com>
2026-09-06 22:07:05 -0500

    Updated DAQ stuff and added more pictures

--- projects.js
+++ projects.js
@@ -3,14 +3,16 @@ const projectsData = [
     title: "Tire Stiffness Rig",
     showcase: true,
     tags: ["Structural Analysis", "Mechanical Design", "FEA", "Testing/Validation"],
-    description: "End to End development of platform intended to give suspension team validated data on tire stiffness values under different psi ranges. Went through multi-iterative design, from initial concept CAD and rendering, where actuation was done through hydraulic jack set up and sensor driven data procurement. After error analysis and cost concerns, design was changed to utilize a pneumatic actuation method and much tighter clearances to save on material cost and decrease deflection error due to bending. V4 is currently slated to be production ready, with a circular tubing design and gussets, with data procurement done primarily through analog means. CAD was done is SolidWorks, and FEA was done in SolidWorks and Ansys.",
+    description: "End to End development of platform intended to give suspension team validated data on tire stiffness values under different psi ranges. Went through multi-iterative design, from initial concept CAD and rendering, where actuation was done through hydraulic jack set up and sensor driven data procurement. After error analysis and cost concerns, design was changed to utilize a pneumatic actuation method and much tighter clearances to save on material cost and decrease deflection error due to bending. V4 is currently slated to be ready for production in mid september, with a circular tubing design and gussets, with data procurement done primarily through analog means. CAD was done is SolidWorks, and FEA was done in SolidWorks and Ansys.",
     images: [
       { src: "assets/project1/main.jpg", alt: "Mechanical Assembly Render (1)" },
-      { src: "assets/project1/slide1.jpg", alt: "Mechanical Assembly Render (2)" },
-      { src: "assets/project1/slide2.png", alt: "SolidWorks Assembly" },
+      { src: "assets/project1/slide1.jpg", alt: "V1 CAD model" },
+      { src: "assets/project1/slide2.png", alt: "Mechanical Assembly Render" },
       { src: "assets/project1/slide3.png", alt: "Data Flow Diagram" },
       { src: "assets/project1/slide5.jpg", alt: "FEA (deformation)" },
-      { src: "assets/project1/slide8.png", alt: "ToF Calibration Analysis" }
+      { src: "assets/project1/slide8.png", alt: "ToF Calibration Analysis" },
+      { src: "assets/project1/slide9.png", alt: "V4 CAD model" },
+      { src: "assets/project1/slide10.png", alt: "V4 deformotion FEA" }
     ]
   },
   {
@@ -21,28 +23,36 @@ const projectsData = [
     images: [
       { src: "assets/project2/main.png", alt: "Accelerated DAQ Schematic" },
       { src: "assets/project2/slide1.png", alt: "Node Casing CAD" },
-      { src: "assets/project2/slide2.png", alt: "Example Data from Initial Testing" },
+      { src: "assets/project2/slide2.png", alt: "Example Data from Initial Testing"},
+      { src: "assets/project2/slide3.png", alt: "Main loop code"},
+      { src: "assets/project2/slide4.png", alt: "DAQ Schematic"}
+
     ]
   },
   {
     title: "26'/27' DAQ Design",
-    showcase: false,
-    tags: ["IoT / ESP32", "Low Power", "Cloud Integration", "CAD Design"],
+    showcase: true,
+    tags: ["CAN FD", "Mechanical Integration", "CAD Design, PCB"],
     description: "Developed an autonomous, solar-powered environmental sensing platform utilizing an ESP32. Implemented deep-sleep optimization routines for multi-month battery life, transmitting processed sensor telemetry via MQTT to a custom cloud dashboard for remote monitoring.",
     images: [
-      { src: "assets/project3/main.jpg", alt: "System Architecture Diagram" },
-      { src: "assets/project3/slide1.jpg", alt: "3D Printed Enclosure CAD (SolidWorks)" },
-      { src: "assets/project3/slide2.jpg", alt: "Live Cloud Dashboard Screenshot" }
+      { src: "assets/project3/main.jpg", alt: "Node Casing Render (1)" },
+      { src: "assets/project3/slide1.jpg", alt: "Node Casing Render (2)" },
+      { src: "assets/project3/slide2.jpg", alt: "CAD" },
+      { src: "assets/project3/slide3.jpg", alt: "CAD Exploded View" },
+      { src: "assets/project3/slide4.jpg", alt: "System Overview" },
+      { src: "assets/project3/slide5.jpg", alt: "Data List" },
+      { src: "assets/project3/slide6.jpg", alt: "Sensor Selection V1" },
+      { src: "assets/project3/slide7.jpg", alt: "Strain Gauge PCB Diagram" },
     ]
   },
   {
     title: "Dual-Core Autonomous Vehicle",
-    showcase: true,
+    showcase: false,
     tags: ["ESP32", "Embedded C", "UDP Protocol", "SolidWorks"],
-    description: "Built a custom remote-operated vehicle platform prioritizing real-time deterministic motor control on Core 0 and low-latency wireless UDP communication processing on Core 1.",
+    description: "Built a custom remote-operated vehicle platform prioritizing real-time deterministic motor control on Core 0 and low-latency wireless UDP communication processing on Core 1. V1 utilized a 10ft long USB cord with software written in MicroPython. V2 went fully wireless using UDP network protocols and software written in C",
     images: [
-      { src: "assets/project4/main.jpg", alt: "Vehicle Chassis Overview" },
-      { src: "assets/project4/slide1.jpg", alt: "Assembly View" }
+      { src: "assets/project4/main.jpg", alt: "" },
+      { src: "assets/project4/slide1.jpg", alt: "" }
     ]
   },
   {
